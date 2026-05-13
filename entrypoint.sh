676926dea17732e3d695d3bfe11aea8101c0a1c3
Aaron Henriquez <aaronx3011@Aarons-MacBook-Pro.local>
2026-05-13 02:21:13 -0400

    Fix entrypoint: filter wg config to strip unsupported directives

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,22 +9,28 @@ mknod /dev/net/tun c 10 200 2>/dev/null || true
 CONFIG=/etc/wireguard/wg0.conf
 
 ip link add dev wg0 type wireguard
-wg setconf wg0 "$CONFIG"
+
+awk '
+/^\[/ { print; next }
+/^(PrivateKey|ListenPort|FwMark|PublicKey|PresharedKey|AllowedIPs|Endpoint|PersistentKeepalive|#|$)/ { print }
+' "$CONFIG" > /tmp/wg.conf
+
+wg setconf wg0 /tmp/wg.conf
 
 ADDRESSES=$(grep -i '^Address' "$CONFIG" | head -1 | sed 's/.*=\s*//')
 OLDIFS=$IFS
 IFS=','
 for addr in $ADDRESSES; do
-    addr=$(echo "$addr" | xargs)
-    ip address add "$addr" dev wg0
+  addr=$(echo "$addr" | xargs)
+  ip address add "$addr" dev wg0
 done
 IFS=$OLDIFS
 
 MTU=$(grep -i '^MTU' "$CONFIG" | head -1 | sed 's/.*=\s*//')
 if [ -n "$MTU" ]; then
-    ip link set mtu "$MTU" up dev wg0
+  ip link set mtu "$MTU" up dev wg0
 else
-    ip link set up dev wg0
+  ip link set up dev wg0
 fi
 
 nginx -g 'daemon off;'
